041ef9c472e6d9aba1b0e68741b971559f116e18
semacitrik <semahrmnc.de@gmail.com>
2024-05-19 12:40:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>MAP PROJECT</h1>
+
+<h3>coded with html,css ,js.</h3>
+
+<p>Leaflet Map</p>
+
+<img src="./Zight Recording 2024-05-19 at 12.33.14 PM.gif">
---
+++ helpers.js
@@ -0,0 +1,48 @@
+export const setStorage = (data) => {
+  
+   const strData = JSON.stringify(data);
+ 
+   localStorage.setItem("notes", strData);
+ };
+ 
+ var carIcon = L.icon({
+   iconUrl: "car.png",
+   iconSize: [50, 60],
+ });
+ var homeIcon = L.icon({
+   iconUrl: "home-marker.png",
+   iconSize: [50, 60],
+ });
+ var jobIcon = L.icon({
+   iconUrl: "job.png",
+   iconSize: [50, 60],
+ });
+ var visitIcon = L.icon({
+   iconUrl: "visit.png",
+   iconSize: [50, 60],
+ });
+ export function detecIcon(type) {
+   switch (type) {
+     case "park":
+       return carIcon;
+     case "home":
+       return homeIcon;
+     case "job":
+       return jobIcon;
+     case "goto":
+       return visitIcon;
+   }
+ }
+ 
+ export const detecType = (type) => {
+   switch (type) {
+     case "park":
+       return "Park Yeri";
+     case "home":
+       return "Ev";
+     case "job":
+       return "İş";
+     case "goto":
+       return "Ziyaret";
+   }
+ };
